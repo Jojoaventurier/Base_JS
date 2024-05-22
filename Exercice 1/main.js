56ecd75be918db20f5187a0866731d84fc8810ba
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-05-22 09:03:20 +0200

    updt

--- Exercice 1/main.js
+++ Exercice 1/main.js
@@ -1,6 +1,29 @@
-const board = document.getElementById("board");
 
-let boxClassName = "";
+/*function showAlert(msgText) {
+
+    alertBox.classList.remove("hide");
+     //msg.innerText = msgText;
+   }
+   
+   function hideAlert(msgText) {
+     
+     alertBox.classList.add("hide");
+     //msg.innerText = msgText;
+   }
+
+
+var d = document,
+  alertBox = d.getElementById("alertBox"),
+  message = d.getElementById("message");
+
+// events
+
+// d.getElementById("btnClose").addEventListener("click", hideAlert );
+*/
+
+const board = document.getElementById("board"); // récupérer la div "board" qui contient notre carré
+
+let boxClassName = ""; //Déclaration et initialisation des variables que l'on va utiliser plus tard pour afficher certains paramètres css
 
 let boxStyle = ""
 let boxBackgroundColor = ""
@@ -12,19 +35,19 @@ let boxFont = ""
 
 let boxStyleArr = []
 
-let box = document.createElement("div")
-box.classList.add("box")
-box.innerText = "Text"
-board.appendChild(box)
+let box = document.createElement("div")  // création de la div pour l'exercice
+box.classList.add("box")    // ajout de la classe .box à la div
+box.innerText = "Text"  // ajout d'un texte au sein de la boîte
+board.appendChild(box)  // matérialiser les boîtes
 
 
-boxStyle = window.getComputedStyle(box);
+boxStyle = window.getComputedStyle(box);  // récupérer tous les attributs css appliqués à la div ".box"
 
-box.onclick = function () {
-    displayBoxStyle()
+box.onclick = function () {   // ajout de l'appel à la fonction displayBoxStyle() quand on clique sur la div
+    displayBoxStyle();
 }
 
-boxClassName = box.className;
+boxClassName = box.className; // on réassigne les variables déclarées pour qu'elles contiennent la valeur des paramètres css que l'on souhaite afficher
 
 boxBackgroundColor = boxStyle.backgroundColor;
 boxTextColor = boxStyle.color
@@ -33,7 +56,7 @@ boxWidth = boxStyle.width
 boxDisplay = boxStyle.display
 boxFont = boxStyle.font
 
-boxStyleArr = [boxBackgroundColor, boxTextColor, boxHeight, boxWidth, boxDisplay, boxFont]
+boxStyleArr = [boxBackgroundColor, boxTextColor, boxHeight, boxWidth, boxDisplay, boxFont] // mise des valeurs des paramètres dans un tableau pour faciliter la manipulation
 
 function displayBoxStyle() {
 
