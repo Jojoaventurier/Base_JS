dd53e19d3f958ac3266e4af6fa89b9bb7c8b83b6
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-05-22 17:00:45 +0200

    updt exo 3

--- Exercice 3/main.js
+++ Exercice 3/main.js
@@ -1,15 +1,42 @@
 
 
-let boxes = document.getElementsByClassName("box");
+let boxes2 = document.getElementsByClassName("box");
+let boxes = document.querySelectorAll(".box");
+let essai = "bonjour"
+let variable = "cliquée"
 
 
-
-for (i = 0; i <= boxes.length; i++) {
-    boxes[i].addEventListener('click', myFunction)
+for (i = 0; i <= boxes.length - 1; i++) {
+    //boxes[i].addEventListener("click", myFunction)
+    boxes[i].setAttribute("id", `${i}`)
+    boxes[i].innerHTML = i
 }
 
-function myFunction() {
-    console.log("hello")
-    console.log(boxes)
-}
 
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+
+        box.classList.toggle("boxClicked")
+
+       /* if( box.classList == "box") {
+            box.classList.add("boxClicked")
+        } else if (box.classList == "box boxClicked") {
+            box.classList.remove("boxClicked")
+        }*/
+    })
+}) 
+
+console.log(boxes)
+console.log(boxes2)
+
+
+
+/*
+const buttons = document.getElementsByTagName("button");
+const buttonPressed = e => {
+  console.log(e.target.id);  // Get ID of Clicked Element
+}
+for (let button of buttons) {
+  button.addEventListener("click", buttonPressed);
+}
+*/
