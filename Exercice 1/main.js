03c793d1787d89545a42437c6cb22c89fde36133
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-05-23 18:48:33 +0200

    completed comments for the Exercice 1

--- Exercice 1/main.js
+++ Exercice 1/main.js
@@ -17,28 +17,28 @@ let boxWidth = ""
 let boxDisplay = ""
 let boxFont = ""
 
-let boxStyleArr = []
+let boxStyleArr = [] // création d'un tableau qui servira à recevoir les propriétés principales que l'on souhaitera afficher dans l'alerte
 
 let box = document.createElement("div")  // création de la div pour l'exercice
 box.classList.add("box")    // ajout de la classe .box à la div
 box.innerText = "Text"  // ajout d'un texte au sein de la boîte
-board.appendChild(box)  // matérialiser les boîtes
+board.appendChild(box)  // matérialiser la boîte au sein de son conteneur
 
 
-boxStyle = window.getComputedStyle(box);  // récupérer tous les attributs css appliqués à la div ".box"
+boxStyle = window.getComputedStyle(box);  // récupérer tous les attributs css de la div "box" et les attribuer à la variable boxStyle
 
 box.addEventListener("click", showAlert)
 
 boxClassName = box.className; // on réassigne les variables déclarées pour qu'elles contiennent la valeur des paramètres css que l'on souhaite afficher
 
-boxBackgroundColor = boxStyle.backgroundColor;
+boxBackgroundColor = boxStyle.backgroundColor; // ici on se sert de boxStyle poru accéder aux éléments récupérés par l'utilisation de la fonction window.getComputedStyle() avec box en paramètre
 boxTextColor = boxStyle.color
 boxHeight = boxStyle.height
 boxWidth = boxStyle.width
 boxDisplay = boxStyle.display
 boxFont = boxStyle.font
 
-boxStyleArr = [boxBackgroundColor, boxTextColor, boxHeight, boxWidth, boxDisplay, boxFont] // mise des valeurs des paramètres dans un tableau pour faciliter la manipulation
+boxStyleArr = [boxBackgroundColor, boxTextColor, boxHeight, boxWidth, boxDisplay, boxFont] // mise des valeurs des propriétés que l'on veut afficher dans le tableau vide créé précédemment pour faciliter la manipulation
 
 messageAffiche = `Classe : .${boxClassName} <br> 
 - Background color : ${boxStyleArr[0]} <br>
